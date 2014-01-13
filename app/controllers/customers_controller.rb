f8d72880e168aeaf0c9b64a5a8ba96d6847fd27f
sudhir <sudorails@gmail.com>
2014-01-13 09:20:23 +0530

    changes

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,7 +4,7 @@ class CustomersController < ApplicationController
   # GET /customers
   # GET /customers.json
   def index
-    @customers = Customer.all
+    @customers = Customer.order("id ASC")
   end
 
   # GET /customers/1
