3b8afd6fa7a989a5449079bbb57a1fdd7b94c960
Sudhir Vishwakarma <sudhirsb2003@gmail.com>
2014-08-13 18:59:45 +0530

    code changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,26 @@
 module ApplicationHelper
 
   def task_list_on_tabs
- 	 Assignment.includes(:tab)
+ 	Assignment.includes(:tab)
+  end
+  
+  def pending_task_on_tab(tab_id)
+    Assignment.joins(:customer).joins(:customer_office).where("assignments.tab_id = ? and customers.status = ?",  tab_id, 'pending')
   end
 
+  def submitted_task_on_tab(tab_id)
+    Assignment.joins(:customer).where("assignments.tab_id = ? and customers.status = ?",  tab_id, 'submitted')
+  end
 
+  def in_progress_task_on_tab(tab_id)
+    Assignment.joins(:customer).where("assignments.tab_id = ? and customers.status = ?",  tab_id, 'completed')
+  end
 
+  def verified_task_on_tab(tab_id)
+    Assignment.joins(:customer).where("assignments.tab_id = ? and customers.status = ?",  tab_id, 'verified')
+  end
 
-
-	def logged_in?
-		!!current_user
-	end
+  def logged_in?
+	!!current_user
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140621075306) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "assignments", force: true do |t|
     t.integer  "tab_id"
     t.integer  "customer_id"
